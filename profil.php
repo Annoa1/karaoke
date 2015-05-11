a6026e34dd32695af6f4f15e112b6aaacdf7f367
Annoa1 <elyse.cordeau@u-psud.fr>
2015-05-11 14:25:43 +0200

    Profil utilisateur

--- profil.php
+++ profil.php
@@ -9,18 +9,49 @@ require_once 'include/fonctions.php';
 $db = db_connexion();
 $userManager = new UserManager($db);
 
+// Protection accès de la page
 if (isset($_GET['id'])) {
   $user = $userManager->get($_GET['id']);
-  if (!$user) {
+  if (!$user)
     go_home();
-  }
-  if ($user->isAdmin()) {
+  if ($user->isAdmin())
     go_home();
-  }
 }
 else {
   go_home();
 }
+
+$modif = false;
+// Modification
+if ($user->isConnected()) {
+
+  // Mail
+  if (isset($_POST['mail'])) {
+    $user->setMail($_POST['mail']);
+    if ($userManager->update($user))
+      $modif = "Ton mail a bien été mis à jour.";
+  }
+
+  // Mot de passe
+  if (isset($_POST['old_pwd']) || isset($_POST['new_pwd1']) || isset($_POST['new_pwd2'])) {
+    if ($_POST['old_pwd'] == $user->pwd()) {
+      if ($_POST['new_pwd1'] == $_POST['new_pwd2']) {
+        $user->setPwd($_POST['new_pwd1']);
+        if ($userManager->update($user))
+          $mdif = "Ton mot-de-passe a bien été mis à jour.";
+      }
+    }
+  }
+
+  // Préférences
+  if (isset($_POST['color'])) {
+    $user->setColor($_POST['color']);
+    if ($userManager->update($user))
+      $modif = "Ta couleur de sous-titres a bien été mise à jour.";
+  }
+
+}
+
 
 ?>
 
@@ -43,18 +74,39 @@ else {
       <h2><?php echo $user->login(); ?></h2>
 
       <?php
-        var_dump($user);
+        if ($user->isConnected()) {
+          if ($modif)
+            echo "<p>".$modif."</p>";
       ?>
 
-      <p>
-        <label>Mail</label>
-          <?php 
-          echo $user->mail(); 
-          if ($user == $_SESSION['user']) { ?>
-        <button>Modifier</button>
-          <?php } 
-          ?>
-      </p>
+      <form method="post" action="profil.php?id=<?php echo $user->id() ?>">
+        <fieldset>
+        <legend>Changer le mail</legend>
+        <label>Mail actuel</label><?php echo $user->mail(); ?><br>
+        <label for="mail">Nouveau mail</label><input id="mail" name="mail" type="text"><br>
+        <input type="submit" value="Modifier">
+        </fieldset>
+      </form>
+
+      <form method="post" action="profil.php?id=<?php echo $user->id() ?>">
+        <fieldset>
+        <legend>Changer le mot-de-passe</legend>
+        <label for="old_pwd">Ancien mot-de-passe</label><input id="old_pwd" name="old_pwd" type="password"><br>
+        <label for="new_pwd1">Nouveau mot-de-passe</label><input id="new_pwd1" name="new_pwd1" type="password"><br>
+        <label for="new_pwd2">Nouveau mot-de-passe (confirmation)</label><input id="new_pwd2" name="new_pwd2" type="password"><br>
+        <input type="submit" value="Modifier">
+        </fieldset>
+      </form>
+
+      <form method="post" action="profil.php?id=<?php echo $user->id() ?>">
+        <fieldset>
+        <legend>Préférences</legend>
+        <label for="color">Sous-titres</label><input id="color" name="color" type="color" value="#<?php echo $user->color(); ?>"><br>
+        <input type="submit" value="Modifier">
+        </fieldset>
+      </form>
+
+      <?php } ?>
 
     </div>
 
